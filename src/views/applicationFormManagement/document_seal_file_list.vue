4429d90b449a438519e0e9efc1c94543e0abd230
zyxiaoxiao <1036237006@qq.com>
2023-07-11 16:03:59 +0800

    骑缝章

---
+++ src/views/applicationFormManagement/document_seal_file_list.vue
@@ -0,0 +1,578 @@
+<template>
+	<div class="all-height flex-column">
+		<el-tabs class="flex-column flex-1 main-card-tabs" v-model="tableTabsValue" type="border-card" @tab-change="tabChange">
+			<el-tab-pane
+				class="main-tab-pane-content all-height flex-column"
+				name="folderinfos"
+				:label="$t('menubasedocumentSeal')"
+				title1="申请盖骑缝章"
+			>
+				<zTable ref="grid_applicationOrderQuery" :tableList="foldersInfosatableList">
+					<template #tableHeaderLleft="scope">
+						<el-button size="small" type="primary" icon="Edit" plain @click="selectAppointment_handler(scope.selectList)">{{
+							$t("menu_stamp")
+						}}</el-button>
+					</template>
+				</zTable>
+			</el-tab-pane>
+			<el-tab-pane
+				class="main-tab-pane-content all-height flex-column"
+				:label="$t('menuselectInternalSubcontractingOrderQuery')"
+				title1="内分包单查询"
+				name="subpackagequery"
+			>
+				<zTable ref="grid_branchapplicationOrderQuery" :tableList="subcontractingtableList">
+					<template #tableHeaderLleft="scope">
+						<el-button size="small" type="primary" icon="Edit" plain @click="selectAppointment_handler(scope.selectList)">{{
+							$t("menu_stamp")
+						}}</el-button>
+					</template>
+				</zTable>
+			</el-tab-pane>
+			<el-tab-pane
+				class="main-tab-pane-content all-height flex-column"
+				:label="$t('menubaseOfficialStampQuery')"
+				title1="骑缝章查询"
+				name="sealquery"
+			>
+				<zTable ref="grid_queryPDFFileInfos" :tableList="uploadPDFFileList"> </zTable>
+			</el-tab-pane>
+		</el-tabs>
+		<div v-dialogStretching>
+			<ZDialog v-model="condobj.dialogShow_documentsealfiledetail" @close="dialogclose" width="45%">
+				<documentsealfile :condobj="condobj"></documentsealfile>
+			</ZDialog>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { ref, reactive, onMounted } from "vue";
+import { useI18n } from "vue-i18n";
+import { GlobalStore } from "/src/store/globalStore.js";
+//弹出报错或者提示框
+import { ElMessage, ElMessageBox } from "element-plus";
+import qs from "qs";
+import http from "@/api/index.js";
+import moment from "moment";
+import zTable from "/src/components/ZTable/index.vue";
+import ZDialog from "/src/components/ZDialog.vue";
+//该骑缝章选择
+import documentsealfile from "@/views/applicationFormManagement/select_seal_file.vue";
+const i18n = useI18n();
+const grid_applicationOrderQuery = ref();
+const grid_branchapplicationOrderQuery = ref();
+const grid_queryPDFFileInfos = ref();
+const globalStore = GlobalStore();
+let userInfo = globalStore.userInfo;
+const tableTabsValue = ref("folderinfos");
+// 父组件传入的参数
+const props = defineProps({
+	condobj: Object
+});
+const condobj = reactive({
+	cond: {},
+	objlist: {}
+});
+
+let foldertype = [
+	{
+		value: "1",
+		label: i18n.t("itemtitleMainAccountOrNot1")
+	},
+	{
+		value: "0",
+		label: i18n.t("itemtitleMainAccountOrNot0")
+	}
+];
+
+//页面初始化渲染完成执行
+onMounted(() => {
+	grid_applicationOrderQuery.value.getTableList();
+});
+
+// 选择申请单盖骑缝章
+const selectAppointment_handler = async selectlist => {
+	if (selectlist != null && selectlist.length < 1) {
+		ElMessage.warning(i18n.t("Message_PleseSelectRecord"));
+		return;
+	}
+	let fdata = selectlist[0];
+	if (fdata && fdata.folderno) {
+		let obj = { freezeflag: "0" };
+		let params = {
+			jsonString: JSON.stringify({ cond: obj })
+		};
+		const res = await http.post("/core/user/user!selectDepartmentSealType.action", qs.stringify(params));
+		if (res) {
+			let v_saleList = [];
+			let data = res.userInfoMaps;
+			if (data) {
+				let map = {};
+				let str = "";
+				for (let sales of data) {
+					str = sales.SALESNAME;
+					map = { label: i18n.t(str.replaceAll(".", "").replace("(", "").replace(")", "")), value: str };
+					if (str == "menu.base.DGMD_PagingSeal_OLD") {
+						if (userInfo.usercode == "801662") {
+							v_saleList.push(map);
+						}
+					} else {
+						v_saleList.push(map);
+					}
+				}
+				condobj.cond = {
+					v_saleList: v_saleList,
+					fdata: fdata
+				};
+				condobj.dialogShow_documentsealfiledetail = true;
+			}
+		}
+	}
+};
+
+//子页面关闭后的方法可以给父页面赋值等操作
+const dialogclose = () => {
+	if (condobj && condobj.cond) {
+		//选择客户的关闭窗口后的事件
+		if (condobj.cond.html && condobj.objlist) {
+			if (condobj.cond.html == "dialogShow_billappointmentdetail") {
+				//关闭后刷新数据
+				if (writeoffidto) {
+					getwriteoffInfo({ writeoffid: writeoffidto });
+					//传参后会自动调用接口刷新
+					tableListInvoices.httpAttribute.baseParams["cond.writeoffid"] = writeoffidto;
+					grid_invoiceInfos.value.reuseTableList();
+					//传参后会自动调用接口刷新
+					tableListFolders.httpAttribute.baseParams["cond.writeoffid"] = writeoffidto;
+					grid_appointmentInfos.value.reuseTableList();
+				}
+			}
+		}
+	}
+};
+
+//切换tab时触发
+const tabChange = targetName => {
+	if (targetName == "folderinfos") {
+		//申请盖骑缝章
+		grid_applicationOrderQuery.value.getTableList();
+	} else if (targetName == "subpackagequery") {
+		//分包单查询
+		grid_branchapplicationOrderQuery.value.getTableList();
+	} else if (targetName == "sealquery") {
+		//骑缝章查询
+		grid_queryPDFFileInfos.value.getTableList();
+	}
+};
+//表格对象申请盖骑缝章
+const foldersInfosatableList = reactive({
+	id: "/comprehensiveQuery/document_seal_file_list.vue_grid_applicationOrderQuery",
+	//设置 为单选
+	isRadio: false,
+	//请求属性设置
+	httpAttribute: {
+		url: "/crm/folders/folders!selectFolderInfoByDocumentSeal.action",
+		root: "foldersInfos",
+		baseParams: { "cond.user_isdept": "Y" }
+	},
+	//表格表头
+	tableColumns: [
+		{
+			type: "selection",
+			width: "40"
+		},
+		{
+			title: "申请单号",
+			label: "columntolockapplynum",
+			prop: "folderno",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "发票编号",
+			label: "itemtitleinvoiceinvoicecode",
+			prop: "invoiceno",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "报价单编号",
+			label: "crmcolumnreservnum",
+			prop: "quotationno",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "盖骑缝章次数",
+			label: "menucolumnTimesOfSealCovering",
+			prop: "writeoffstatus",
+			type: "Input",
+			width: "140"
+		},
+		{
+			title: "业务人员",
+			label: "tablesenametitle",
+			prop: "sename",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "部门",
+			label: "corpinfopaneldepartmentcodetitle",
+			prop: "dept",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "客户号",
+			label: "fieldcolumncustomercode",
+			prop: "rasclientid",
+			type: "Input",
+			width: "150"
+		},
+		{
+			title: "客户名称",
+			label: "columnCustomerName",
+			prop: "compname",
+			type: "Input",
+			width: "260"
+		},
+		{
+			title: "金额",
+			label: "columnappointmentothercostcostprice",
+			prop: "currency",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "币种",
+			label: "columnappointmentdesc53",
+			prop: "currencyname",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "中文样本名",
+			label: "columnbaseappointmentChineseForSample",
+			prop: "samplename",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "英文样本名",
+			label: "columnbaseappointmentEnglishSampleName",
+			prop: "samplename_en",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "样品描述",
+			label: "columnSampleInformation",
+			prop: "sampleinformation",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "申请人编码",
+			label: "columnApplicantcode",
+			prop: "cscode",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "申请人名称",
+			label: "columnApplicantdesc",
+			prop: "csname",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "申请时间",
+			label: "i18ncrmcontractApplicationDate",
+			prop: "drawdate",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "销售代理编号",
+			label: "corpinfopanelqydlsbmtitle",
+			prop: "agentno",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "入单日期",
+			label: "panelcolumnLoginDate",
+			prop: "drawdate",
+			type: "Input",
+			width: "160"
+		}
+	],
+	// 表格数据
+	tableData: []
+});
+
+//表格对象内分包单查询
+const subcontractingtableList = reactive({
+	id: "/comprehensiveQuery/document_seal_file_list.vue_grid_branchapplicationOrderQuery",
+	//设置 为单选
+	isRadio: false,
+	//请求属性设置
+	httpAttribute: {
+		url: "/crm/folders/folders!selectFolderInfoByDocumentSeal.action",
+		root: "foldersInfos",
+		baseParams: { "cond.branch_dept": "Y" }
+	},
+	//表格表头
+	tableColumns: [
+		{
+			type: "selection",
+			width: "40"
+		},
+		{
+			title: "申请单号",
+			label: "columntolockapplynum",
+			prop: "folderno",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "发票编号",
+			label: "itemtitleinvoiceinvoicecode",
+			prop: "invoiceno",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "报价单编号",
+			label: "crmcolumnreservnum",
+			prop: "quotationno",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "盖骑缝章次数",
+			label: "menucolumnTimesOfSealCovering",
+			prop: "writeoffstatus",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "业务人员",
+			label: "tablesenametitle",
+			prop: "sename",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "部门",
+			label: "corpinfopaneldepartmentcodetitle",
+			prop: "dept",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "客户号",
+			label: "fieldcolumncustomercode",
+			prop: "rasclientid",
+			type: "Input",
+			width: "150"
+		},
+		{
+			title: "客户名称",
+			label: "columnCustomerName",
+			prop: "compname",
+			type: "Input",
+			width: "260"
+		},
+		{
+			title: "金额",
+			label: "columnappointmentothercostcostprice",
+			prop: "currency",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "币种",
+			label: "columnappointmentdesc53",
+			prop: "currencyname",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "中文样本名",
+			label: "columnbaseappointmentChineseForSample",
+			prop: "samplename",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "英文样本名",
+			label: "columnbaseappointmentEnglishSampleName",
+			prop: "samplename_en",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "样品描述",
+			label: "columnSampleInformation",
+			prop: "sampleinformation",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "申请人编码",
+			label: "columnApplicantcode",
+			prop: "cscode",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "申请人名称",
+			label: "columnApplicantdesc",
+			prop: "csname",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "申请时间",
+			label: "i18ncrmcontractApplicationDate",
+			prop: "drawdate",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "销售代理编号",
+			label: "corpinfopanelqydlsbmtitle",
+			prop: "agentno",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "入单日期",
+			label: "panelcolumnLoginDate",
+			prop: "drawdate",
+			type: "Input",
+			width: "160"
+		}
+	],
+	// 表格数据
+	tableData: []
+});
+
+//表格对象骑缝章查询
+const uploadPDFFileList = reactive({
+	id: "/comprehensiveQuery/document_seal_file_list.vue_grid_queryPDFFileInfos",
+	//设置 为单选
+	isRadio: false,
+	//请求属性设置
+	httpAttribute: {
+		url: "/core/uploadnew/upload!selectUploadPDFFileByCond.action",
+		root: "uploadPDFFileList",
+		baseParams: {}
+	},
+	//表格表头
+	tableColumns: [
+		{
+			type: "selection",
+			width: "40"
+		},
+		{
+			title: "客户号",
+			label: "fieldcolumncustomercode",
+			prop: "corpcode",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "客户名称",
+			label: "columnCustomerName",
+			prop: "corpdesc",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "申请单号",
+			label: "columntolockapplynum",
+			prop: "folderno",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "用印类型",
+			label: "columnSealtypetitle",
+			prop: "sealname",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "是否加密",
+			label: "menubaseIsEncryption",
+			prop: "isencryption",
+			type: "Select",
+			width: "140",
+			typeData: foldertype
+		},
+		{
+			title: "盖骑缝章次数",
+			label: "menucolumnTimesOfSealCovering",
+			prop: "foldernum",
+			type: "Input",
+			width: "150"
+		},
+		{
+			title: "部门",
+			label: "corpinfopaneldepartmentcodetitle",
+			prop: "dept",
+			type: "Input",
+			width: "140"
+		},
+		{
+			title: "文件名称",
+			label: "columnjxFileName",
+			prop: "filename",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "文件大小",
+			label: "itemtitleyingjifilesize",
+			prop: "filesize",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "创建人",
+			label: "hotlinkpanelcjrbmtitle",
+			prop: "recordercode",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "创建人名称",
+			label: "i18ncrmcontractCreatePersonName",
+			prop: "recorderdesc",
+			type: "Input",
+			width: "160"
+		},
+		{
+			title: "创建时间",
+			label: "corpinfopanelqycjsjtitle",
+			prop: "recordtime",
+			type: "Input",
+			width: "160"
+		}
+	],
+	// 表格数据
+	tableData: []
+});
+</script>
+
+<style scoped lang="scss">
+:deep(.el-tabs__content) {
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+}
+</style>
---
+++ src/views/applicationFormManagement/select_seal_file.vue
@@ -0,0 +1,259 @@
+<template>
+	<div class="all-height flex-column">
+		<div style="margin-top: 10px" class="flex-1">
+			<el-alert type="info" :title="$t('Message_OperationTip')" show-icon description="" :closable="false">
+				<template #default>
+					<div>
+						{{ $t("Statement_filesize") }} 20 MB
+						<br />
+						{{ $t("Statement_filetype") }}
+						pdf
+					</div>
+				</template>
+			</el-alert>
+			<el-form style="margin: 0px 15px" label-position="right" label-width="120px" :model="sformData" ref="ruleFormRef">
+				<el-divider title1="上传附件" content-position="left">{{ $t("itemtitleyingjiupfile") }}</el-divider>
+				<el-row class="main-align-items-center">
+					<el-col :span="24">
+						<el-form-item :label="$t('fieldcolumncustomercode') + ':'" title1="客户号">
+							<el-input type="text" v-model="sformData.corpcode" readonly></el-input>
+						</el-form-item>
+					</el-col>
+					<el-col :span="24">
+						<el-form-item :label="$t('columnCustomerName') + ':'" title1="客户名称">
+							<el-input type="text" v-model="sformData.corpdesc" readonly></el-input>
+						</el-form-item>
+					</el-col>
+					<el-col :span="24">
+						<el-form-item :label="$t('columntolockapplynum') + ':'" title1="申请单号">
+							<el-input type="text" v-model="sformData.folderno" readonly></el-input>
+						</el-form-item>
+					</el-col>
+					<el-col :span="24">
+						<el-form-item :label="$t('columnSealtypetitle') + ':'" title1="用印类型">
+							<el-select
+								v-model="sformData.sealname"
+								class="full-width-input"
+								filterable
+								placeholder="Select"
+							>
+								<el-option v-for="item in v_saleList" :key="item.value" :label="item.label" :value="item.value" />
+							</el-select>
+						</el-form-item>
+					</el-col>
+					<el-col :span="24">
+						<el-form-item :label="$t('menubasePagingSealNumber') + ':'" title1="骑缝章数量">
+							<el-select
+								v-model="sformData.sealnumber"
+								class="full-width-input"
+								filterable
+								placeholder="Select"
+							>
+								<el-option v-for="item in numberlist" :key="item.value" :label="item.label" :value="item.value" />
+							</el-select>
+						</el-form-item>
+					</el-col>
+					<el-col :span="24">
+						<el-form-item :label="$t('menubaseIsEncryption') + ':'" title1="是否加密">
+							<el-select
+								v-model="sformData.isencryption"
+								class="full-width-input"
+								filterable
+								placeholder="Select"
+							>
+								<el-option v-for="item in encryption" :key="item.value" :label="item.label" :value="item.value" />
+							</el-select>
+						</el-form-item>
+					</el-col>
+				</el-row>
+			</el-form>
+			<el-upload
+				style="margin-left: 120px; margin-right: 40px"
+				ref="upload"
+				:action="globalStore.serverUrl + '/core/uploadnew/upload!uploadfile.action'"
+				:limit="1"
+				:on-exceed="handleExceed"
+				:beforeUpload="handleBeforeUpload"
+				:on-error="handleError"
+				:data="upload_data"
+				:auto-upload="false"
+                :http-request="httprequestfun"
+                :accept="['pdf','PDF']"
+			>
+				<template #trigger>
+					<el-button icon="Document" type="primary">{{ $t("UPLOAD_selectFile") }}</el-button>
+				</template>
+				<el-button style="margin-left: 20px; margin-bottom: 8px" type="success" icon="UploadFilled" @click="submitUpload">
+					{{ $t("SRM_stamp") }}
+				</el-button>
+			</el-upload>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { ref, reactive, onMounted } from "vue";
+import { useI18n } from "vue-i18n";
+import qs from "qs";
+import http from "@/api/index.js";
+import { genFileId, ElMessage } from "element-plus";
+import { GlobalStore } from "/src/store/globalStore.js";
+//import download from 'downloadjs'
+const globalStore = GlobalStore();
+
+const i18n = useI18n();
+
+// 父组件传入的参数
+const props = defineProps({
+	condobj: Object
+});
+
+//提佣上传附件初始化信息
+const sformData = reactive({
+	corpcode: "",
+	corpdesc: "",
+	folderno: "",
+	sealname: "",
+	sealnumber: "",
+	isencryption: "",
+	filename: ""
+});
+let v_saleList = reactive([]);
+
+//是否加密
+let encryption = [
+    {
+		value: "0",
+		label: i18n.t("itemtitleMainAccountOrNot0")
+	},
+	{
+		value: "1",
+		label: i18n.t("itemtitleMainAccountOrNot1")
+	}	
+];
+
+//骑缝章数量
+const numberlist = [
+	{
+		value: "1",
+		label: "1"
+	},
+	{
+		value: "2",
+		label: "2"
+	},
+	{
+		value: "3",
+		label: "3"
+	},
+	{
+		value: "4",
+		label: "4"
+	},
+	{
+		value: "5",
+		label: "5"
+	}
+];
+
+const upload_data = reactive({});
+const upload = ref();
+
+//当超出限制时，执行的钩子函数，在这里就是当触发了limit后执行
+const handleExceed = files => {
+	upload.value.clearFiles();
+	const file = files[0];
+	file.uid = genFileId();
+	upload.value.handleStart(file);
+};
+
+const httprequestfun = async (params) => {
+    let f = new FormData();//表单数据以及文件
+    f.append("file",params.file);
+    let filename = params.file.name;
+    f.append("jsonString",params.data.jsonString);
+    const res = await http.post("/core/uploadnew/upload!uploadfile.action", f,{
+			responseType: "blob"
+		});
+	if(res){
+        exportDownload( res, filename); 
+        props.condobj.dialogShow_documentsealfiledetail = false;     
+    }
+    
+};
+
+//上传
+const submitUpload = () => {
+	if (sformData.folderno && sformData.sealname) {
+		upload.value.submit();
+	} else {
+		ElMessage({
+			type: i18n.t("Message_OperationTip"),
+			message: i18n.t("errormsgerr8")
+		});
+	}
+};
+
+//发送请求前执行的函数，在这里可以做一些条件判断，配置参数等
+const handleBeforeUpload = file => {
+    if(file.name){
+        let fileName = file.name;
+        fileName = fileName.toLowerCase();
+        if(fileName.indexOf("pdf") < 0){
+            ElMessage.warning("上传需要PDF文件!");
+		    return false;
+        }
+        if(fileName.length > 200){
+            ElMessage.warning("Statement_longname");
+		    return false;
+        }
+    }
+    sformData.filename = file.name;//文件名称
+	upload_data.jsonString = JSON.stringify({ uploadPDFFileInfo: sformData });
+
+	return true;
+};
+
+//页面初始化渲染完成执行
+onMounted(() => {
+	if(props.condobj){
+        let list = props.condobj.cond.v_saleList;
+        let fdata = props.condobj.cond.fdata;
+        if(list != null && list.length > 0){
+            for(let sale of list){
+                v_saleList.push(sale);
+            }
+        }
+        if(fdata.folderno){
+            sformData.corpcode = fdata.rasclientid;
+            sformData.corpdesc = fdata.compname;
+            sformData.folderno = fdata.folderno;
+            sformData.sealnumber = "1";
+            sformData.isencryption = "1";
+        }
+    }
+});
+
+const exportDownload = async (data,fileName) => {
+    //执行下载,{type : 'application/pdf;charset=utf-8''}
+    // let b = new Blob([data]);
+    let url = window.URL.createObjectURL(data);    
+    let link = document.createElement('a');
+    link.download = fileName;
+    link.style.display = 'none';
+    link.href = url;
+    //link.setAttribute('download',fileName + '.' + fileFormat);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(link.href);
+
+};
+
+//请求失败后执行的函数，相当于axios的catch
+const handleError = error => {
+	ElMessage.warning(i18n.t("Statement_failup") + error.errors + "！");
+};
+</script>
+
+<style></style>
